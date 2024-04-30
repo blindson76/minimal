6276f3dd0af1a2fe5ff778820d72f34b369142b5
blnidson76 <blnidson76.gmail.com>
2024-04-30 14:53:06 +0300

    disk util

--- src/minimal_overlay/bundles/ldr-service/hivex.go
+++ src/minimal_overlay/bundles/ldr-service/hivex.go
@@ -2,20 +2,31 @@ package main
 
 /*
 #cgo CFLAGS:-I/home/user/work/mini/src/work/tmp_rootfs/include
-#cgo LDFLAGS:-L/home/user/work/mini/src/work/tmp_rootfs/lib -lparted -lhivex
+#cgo LDFLAGS:-L/home/user/work/mini/src/work/tmp_rootfs/lib -lhivex
 #include <hivex.h>
-
-
 */
 import "C"
 import (
+	"errors"
+	"fmt"
+	"runtime"
 	"unsafe"
+
+	"github.com/google/uuid"
 )
 
 type Hive struct {
 	file string
 	hiv  *C.hive_h
 	n    C.hive_node_h
+}
+type HiveValue struct {
+	hiv   *C.hive_h
+	n     C.hive_node_h
+	pKey  *C.char
+	pVal  C.hive_value_h
+	pType C.hive_type
+	pSize C.size_t
 }
 
 func (h *Hive) Childs() []*Hive {
@@ -33,6 +44,52 @@ func (h *Hive) Childs() []*Hive {
 	}
 	return childs
 }
+func (h *Hive) Value(key string) *HiveValue {
+	kv, err := C.hivex_node_get_value(h.hiv, h.n, C.CString(key))
+	if err != nil {
+		panic(err)
+	}
+	var vType C.hive_type
+	var vSize C.size_t
+	res := C.hivex_value_type(h.hiv, kv, &vType, &vSize)
+	if res != 0 {
+		panic(err)
+	}
+	pv := &HiveValue{
+		hiv:   h.hiv,
+		n:     h.n,
+		pVal:  kv,
+		pType: vType,
+		pSize: vSize,
+	}
+	pv.pKey = C.hivex_value_key(h.hiv, kv)
+	return pv
+}
+func (hv *HiveValue) GetBytes() []byte {
+	pval, err := C.hivex_value_value(hv.hiv, hv.pVal, &hv.pType, &hv.pSize)
+	if err != nil {
+		panic(err)
+	}
+	data := C.GoBytes(unsafe.Pointer(pval), C.int(hv.pSize))
+	return data
+}
+func (hv *HiveValue) SetByte(value []byte) error {
+
+	var pVal C.hive_set_value
+	pinner := &runtime.Pinner{}
+	cPoint := unsafe.Pointer(&value[0])
+	pinner.Pin(cPoint)
+	defer pinner.Unpin()
+	pVal.key = hv.pKey
+	pVal.value = (*C.char)(cPoint)
+	pVal.len = C.size_t(len(value))
+	pVal.t = hv.pType
+	res := C.hivex_node_set_value(hv.hiv, hv.n, &pVal, 0)
+	if res == 0 {
+		return nil
+	}
+	return errors.New(fmt.Sprintf("Couldn't set value:%d", res))
+}
 func (h *Hive) Val(key string) []byte {
 	kv, err := C.hivex_node_get_value(h.hiv, h.n, C.CString(key))
 	if err != nil {
@@ -46,6 +103,21 @@ func (h *Hive) Val(key string) []byte {
 	}
 	data := C.GoBytes(unsafe.Pointer(pval), C.int(vSize))
 	return data
+}
+func (h *Hive) SetVal(key string, value []byte) error {
+	var pVal C.hive_set_value
+	pinner := &runtime.Pinner{}
+	cPoint := unsafe.Pointer(&value[0])
+	pinner.Pin(cPoint)
+	defer pinner.Unpin()
+	pVal.key = C.CString(key)
+	pVal.value = (*C.char)(cPoint)
+	pVal.len = C.size_t(len(value))
+	res := C.hivex_node_set_value(h.hiv, h.n, &pVal, 0)
+	if res == 0 {
+		return nil
+	}
+	return errors.New(fmt.Sprintf("Couldn't set value of hive:%d", res))
 }
 func (h *Hive) ValStr(key string) string {
 	kv, err := C.hivex_node_get_value(h.hiv, h.n, C.CString(key))
@@ -110,11 +182,17 @@ func (h *Hive) Name() string {
 	}
 	return C.GoString(pName)
 }
+func (h *Hive) Commit() error {
+	if C.hivex_commit(h.hiv, nil, 0) == 0 {
+		return nil
+	}
+	return errors.New("Failed to write hive")
+}
 func OpenHive(path string) *Hive {
 	self := &Hive{
 		file: path,
 	}
-	hnd, err := C.hivex_open(C.CString(path), 0)
+	hnd, err := C.hivex_open(C.CString(path), C.HIVEX_OPEN_WRITE)
 	if err != nil {
 		panic(err)
 	}
@@ -125,4 +203,51 @@ func OpenHive(path string) *Hive {
 	self.n = root
 	self.hiv = hnd
 	return self
+}
+func printUUID(data []byte) {
+	uid, err := uuid.FromBytes(data)
+	if err != nil {
+		return
+	}
+	fmt.Println(uid)
+}
+func BCDFix(bxdStore, device string) error {
+
+	diskName, partId := GetDiskAndPartNum(device)
+	fmt.Println(diskName, partId)
+	dev := GetDevice(diskName)
+	if dev == nil {
+		panic("Couldn' open dev")
+	}
+	disk := dev.GetDisk()
+	part := disk.GetPartition(partId)
+	diskUUID := disk.UUID()
+	partUUID := part.UUID()
+	h := OpenHive(bxdStore).GetChild("Objects")
+	printUUID(diskUUID)
+	printUUID(partUUID)
+	fixed := 0
+	for _, c := range h.Childs() {
+		for _, field := range c.GetChild("Elements").Childs() {
+			if field.Name() == "11000001" || field.Name() == "21000001" {
+				kv := field.Value("Element")
+				fieldVal := kv.GetBytes()
+				newValue := make([]byte, len(fieldVal))
+				copy(newValue, fieldVal)
+				copy(newValue[32:], EncodeUUID(partUUID))
+				copy(newValue[56:], EncodeUUID(diskUUID))
+				kv.SetByte(newValue)
+				err := field.SetVal("Element", newValue)
+				if err != nil {
+					panic(err)
+				}
+				fixed += 1
+
+			}
+		}
+	}
+	if fixed == 2 {
+		return h.Commit()
+	}
+	return errors.New("Unexpected result")
 }
--- src/minimal_overlay/bundles/ldr-service/parted.go
+++ src/minimal_overlay/bundles/ldr-service/parted.go
@@ -2,17 +2,15 @@ package main
 
 /*
 #cgo CFLAGS:-I/home/user/work/mini/src/work/tmp_rootfs/include
-#cgo LDFLAGS:-L /home/user/work/mini/src/work/tmp_rootfs/lib -lparted
+#cgo LDFLAGS:-L /home/user/work/mini/src/work/tmp_rootfs/lib -lparted -ludev
+#include <sys/stat.h>
+#include <libudev.h>
 #include <parted/parted.h>
-PedDevice* getDevice(char* dev){
-	ped_device_probe_all();
-	ped_device_get(dev);
-}
-
 */
 import "C"
 import (
 	"fmt"
+	"strings"
 	"unsafe"
 )
 
@@ -21,6 +19,9 @@ type PedDisk struct {
 }
 type PedDevice struct {
 	pDev *C.PedDevice
+}
+type PedPartition struct {
+	pPart *C.PedPartition
 }
 
 func (p *PedDevice) GetDisk() *PedDisk {
@@ -40,8 +41,29 @@ func (d *PedDisk) UUID() []byte {
 	data := C.GoBytes(unsafe.Pointer(uid), 16)
 	return data
 }
+func (d *PedDisk) GetPartition(partNum int) *PedPartition {
+	pPart := C.ped_disk_get_partition(d.pDisk, C.int(partNum))
+	if pPart == nil {
+		return nil
+	}
+	return &PedPartition{
+		pPart: pPart,
+	}
+}
+
+func (p *PedPartition) UUID() []byte {
+	uid := C.ped_partition_get_uuid(p.pPart)
+	if uid == nil {
+		return nil
+	}
+	data := C.GoBytes(unsafe.Pointer(uid), 16)
+	return data
+}
 func GetDevice(dev string) *PedDevice {
 	pDev := C.ped_device_get(C.CString(dev))
+	if pDev == nil {
+		return nil
+	}
 	return &PedDevice{
 		pDev: pDev,
 	}
@@ -63,5 +85,54 @@ func Devices() []string {
 		}
 	}
 	fmt.Println(devs)
+	return nil
+}
+
+func GetDiskAndPartNum(part string) (string, int) {
+	var stat C.struct_stat
+	ret := C.fstatat(0, C.CString(part), &stat, 0)
+	if ret != 0 {
+		panic(fmt.Sprintf("Dev: %s not found", part))
+	}
+	udev := C.udev_new()
+	dev := C.udev_device_new_from_devnum(udev, 'b', stat.st_rdev)
+	if dev == nil {
+		panic("Not found dev")
+	}
+	pdev := C.udev_device_get_parent(dev)
+	if pdev == nil {
+		panic("Not found parent dev")
+	}
+	pDevNum := C.udev_device_get_devnum(pdev)
+	if pDevNum == 0 {
+		panic("Not found parent dev id")
+	}
+	devName := C.GoString(C.udev_device_get_devnode(dev))
+	pDevName := C.GoString(C.udev_device_get_devnode(pdev))
+	return strings.Replace(part, devName, pDevName, 1), int(stat.st_rdev - pDevNum)
+}
+
+func create_part(disk *C.PedDisk, fs string, size C.longlong) *C.PedPartition {
+	//C.ped_partition_new()
+	pAlign := C.ped_disk_get_partition_alignment(disk)
+	if pAlign == nil {
+		panic("couldn't get alignment")
+	}
+
+	sectorSize := C.ped_unit_get_size(disk.dev, C.PED_UNIT_SECTOR)
+	start := C.longlong(0)
+	end := start + size/sectorSize
+	pedFS := C.ped_file_system_type_get(C.CString(fs))
+	if pedFS == nil {
+		panic("Fs not found")
+	}
+	newPart := C.ped_partition_new(disk, C.PED_PARTITION_NORMAL, pedFS, start, end)
+	if newPart == nil {
+		panic("partition couldn't be created")
+	}
+	pedConstraint := C.ped_constraint_any(disk.dev)
+	res := C.ped_disk_add_partition(disk, newPart, pedConstraint)
+
+	fmt.Println("Unit fmt:", sectorSize, start, end, res)
 	return nil
 }
