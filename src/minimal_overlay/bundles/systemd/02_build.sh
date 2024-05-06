488192526eb66d25bcf064dbfc86abe2672b2eec
blnidson76 <blnidson76.gmail.com>
2024-05-06 09:04:49 +0300

    disk util

---
+++ src/minimal_overlay/bundles/systemd/02_build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+. ../../common.sh
+
+cd $WORK_DIR/overlay/$BUNDLE_NAME
+
+# Change to the Dropbear source directory which ls finds, e.g. 'dropbear-2016.73'.
+cd $(ls -d systemd-*)
+ls 
+exit
+if [ -f Makefile ] ; then
+  echo "Preparing $BUNDLE_NAME work area. This may take a while."
+  make -j $NUM_JOBS clean
+else
+  echo "The clean phase for $BUNDLE_NAME has been skipped."
+fi
+
+
+echo "Building $BUNDLE_NAME."
+LDFLAGS="-L$TMP_ROOTFS/lib -L$TMP_ROOTFS/usr/lib -static" \
+  CFLAGS="-I$TMP_ROOTFS/include -I$TMP_ROOTFS/usr/include" \
+  PKG_CONFIG_SYSROOT_DIR=${TMP_ROOTFS} \
+  PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 \
+  PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 \
+  PKG_CONFIG_PATH=$TMP_ROOTFS/lib/pkgconfig:$TMP_ROOTFS/usr/lib/pkgconfig \
+  PKG_CONFIG_LIBDIR=$TMP_ROOTFS/lib/pkgconfig:$TMP_ROOTFS/usr/lib/pkgconfig \
+  make sgdisk -j $NUM_JOBS
+echo done
+
+
+cp -r --remove-destination ./sgdisk \
+  $OVERLAY_ROOTFS/bin/sgdisk
+echo "Bundle $BUNDLE_NAME has been installed."
+
+cd $SRC_DIR
