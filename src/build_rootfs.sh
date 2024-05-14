ce8401af753f33fbb9aa4f051e1802c9932c46f6
blnidson76 <blnidson76.gmail.com>
2024-05-14 16:49:08 +0300

    init

--- src/build_rootfs.sh
+++ src/build_rootfs.sh
@@ -4,6 +4,11 @@ pushd ./minimal_overlay/bundles/ldr-service
 go build .
 popd
 cp -rf ./minimal_overlay/bundles/ldr-service/ldr ./work/overlay_rootfs/bin/ldr
+
+pushd /media/sf_work/hloader/
+go build .
+popd
+cp -rf /media/sf_work/hloader/hloader ./work/overlay_rootfs/bin/hloader
 ./09_generate_rootfs.sh
 ./10_pack_rootfs.sh
 cp -rf ./work/rootfs.cpio.xz ~/work/pxe/root/rootfs.xz
