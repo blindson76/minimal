83b59f1e263add5caf7434c568f426381698e96e
blindson76 <blindson76@gmail.com>
2024-12-10 22:28:29 +0300

    Update build_from.sh

--- src/build_from.sh
+++ src/build_from.sh
@@ -3,7 +3,7 @@
 set -e
 start=$1-0
 for script in $(ls | grep '^[0-9]*_.*.sh'); do
-  if [[ 10#${script:0:2} -ge $1 ]] && [[ 10#${script:0:2} -lt 16 ]]
+  if [[ 10#${script:0:2} -ge $1 ]] && [[ 10#${script:0:2} -lt 11 ]]
     then
       ./$script
   fi
